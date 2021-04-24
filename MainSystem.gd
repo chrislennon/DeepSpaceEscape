4b36565c61a1a5bd3e1ca8c2f568135a87a770a6
Chris Lennon <chrislennon@users.noreply.github.com>
2021-04-24 20:06:38 +0200

    lol stuff

--- MainSystem.gd
+++ MainSystem.gd
@@ -11,7 +11,7 @@ var traversalSpeed = traversalDistance/systemClock
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Initialize scene timer
-	get_node("Container/Countdown Time").set_text(String(systemClock))
+	get_node("System Clock/Time").set_text(String(systemClock))
 	
 	# TODO Scene initiation flash prompt X seconds in this system
 	
@@ -22,7 +22,7 @@ func _ready():
 func _process(delta):
 	# Progress scene timer
 	systemClock -=delta
-	get_node("Container/Countdown Time").set_text(String(systemClock))
+	get_node("System Clock/Time").set_text(String(systemClock))
 	
 	if systemClock <= 0:
 		# TODO Scene transition FTL online/animation flash/load next system
--- PlanetObject.gd
+++ PlanetObject.gd
@@ -1,18 +1,12 @@
 extends RigidBody2D
 
-signal clicked
-
-var held = false
-
-#func _input_event(viewport, event, shape_idx):
-#	if event is InputEventMouseButton:
-#		if event.button_index == BUTTON_LEFT and event.pressed:
-#			emit_signal("clicked", self)
+var obtainableResources = {
+	
+}
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-		#if event.type == InputEvent.left_click:
 			print("click")
+			# Pop planet actions GUI here
 	pass
-	
